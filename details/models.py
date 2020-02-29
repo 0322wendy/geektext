6c6c890432af9753c292b21172fd0ad1250a087d
Wendy Orellana <worel001@fiu.edu>
2020-02-28 22:43:18 -0500

    Added comment and rating
    Added comment and rating

--- details/models.py
+++ details/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models import Avg
 
 class Book(models.Model):
     PURCHASE_CHOICES = [('New','New'),('Used','Used')]
@@ -13,4 +15,11 @@ class Book(models.Model):
     cover = models.ImageField(upload_to='uploads/')
     cost = models.FloatField(null=True, blank=True, default=None)
     pages = models.IntegerField(null=True, blank=True, default=None)
-    
+    avg_rating = models.DecimalField(decimal_places=1, max_digits=2, default=0)
+
+    def rating_avg(self):
+        book = Book.objects.get(id=self.id)
+        return list(book.rating_set.aggregate(Avg('rating')).values())[0]
+
+    def rating_count(self):
+        return Rating.objects.all().filter(book=self).count()
--- geektext/urls.py
+++ geektext/urls.py
@@ -5,11 +5,15 @@ from django.conf.urls.static import static # new
 from django.conf.urls import url
 
 from details import views
+from ratings import views as ratings_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name ='home'),
     url(r'^details/(\d+)/', views.book_detail, name='book_detail'),
+    url(r'^reviews/(\d+)/', ratings_views.display_comment, name='display_comment'),
+    url(r'^details/review/(\d+)/', ratings_views.write_comment, name='write_comment'),
+    url(r'^details/rate/(\d+)/', ratings_views.rate_book, name='rate_book'),
 ]
 
 if settings.DEBUG: # new
